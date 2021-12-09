641018479daa68dabe6c991eb41cb17fc9e64645
Joshua John <joshuajohnv@protonmail.com>
2021-12-09 08:34:14 +0530

    Basic script for Lenovo Legion 5. So far, supports toggling conservation mode and usb charging.

---
+++ Lenovo_Legion_5_2020.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+conservation_mode="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+usb_charging="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/usb_charging"
+
+while getopts "cu" options 
+do
+    case "${options}" in
+        c)
+            if (($(echo $(($(cat $conservation_mode) ^ 1)) | sudo tee $conservation_mode) == 1))
+            then
+                echo Conservation mode on
+            else
+                echo Conservation mode off
+            fi
+            ;;
+        u)
+            if (($(echo $(($(cat $usb_charging) ^ 1)) | sudo tee $usb_charging) == 1))
+            then
+                echo USB charging on
+            else
+                echo USB charging off
+            fi
+            ;;
+        \?) echo Invalid option. Please use --help for correct usage.
+            ;;
+    esac
+done
