3ad6484f60bf028e207225135e5ad1087de1d4c0
Joshua John <joshuajohnv@protonmail.com>
2021-12-23 00:57:57 +0530

    Script to rotate display and trackpad coordinates.

---
+++ Display_Trackpad_Orientation_Script.sh
@@ -0,0 +1,25 @@
+#! //usr/bin/env bash
+
+externalMouse="$(xinput list | awk '/Logitech Wireless Mouse/ {print $6}' | awk -F"id=" '{print $2}')"
+trackpad="$(xinput list | awk '/Touchpad/ {print}' | awk -F"id=" '{print $2}' | awk -F" " '{print $1}')"
+
+while getopts "lrni" options
+do
+    case "${options}" in
+        l)
+            xrandr -o left
+            xinput set-prop $trackpad 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
+            ;;
+        r)
+            xrandr -o right
+            xinput set-prop $trackpad 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
+            ;;
+        n)
+            xrandr -o normal
+            xinput set-prop $trackpad 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+            ;;
+        i)
+            xrandr -o inverted
+            xinput set-prop $trackpad 'Coordinate Transformation Matrix' -1 0 1 0 -1 1 0 0 1
+    esac
+done
