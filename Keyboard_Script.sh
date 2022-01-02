900038f9248959718e597822e5c050e89607b31e
Joshua John <joshuajohnv@protonmail.com>
2022-01-02 14:16:35 +0530

    Cleaned up script and refined some commands.

--- Keyboard_Script.sh
+++ Keyboard_Script.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env bash
 
-if kill -9 $(pgrep -f ${BASH_SOURCE[0]} | grep -v $$)
-then
-    echo "Killed previous process."
+if  kill `pgrep -f $0 | grep -v $$`; then
+    echo Killed previous process.
 else
-    echo "No previous process was found."
+    echo No previous process was found.
 fi
 
-internalKeyId="$(xinput list | awk '/keyboard/ {print}' | awk '/(8910)/ {print}' |  awk '/Keyboard/ {print $8}' | awk -F"id=" '{print $2}')"
+bluetooth_key_id=`xinput list | awk '/Keyboard K380 Keyboard/ { print $5 }' | awk -F"=" '{ found=1 } { print $2 } END { if (!found) print "0" }'`
+internal_key_id=`xinput list | awk '/keyboard/ && /8910) Keyboard/ { print $8 }' | awk -F"=" '{ print $2 }'`
 
 clean()
 {
-    xinput enable $internalKeyId
-    echo "Internal keyboard enabled."
+    xinput enable $internal_key_id
+    echo Internal keyboard enabled.
     exit
 }
 
-trap clean SIGHUP SIGINT SIGTERM
+trap clean SIGHUP SIGINT SIGTERM SIGKILL
 
-while [ 1 ]
-do
-    bluetoothKeyId="$(xinput list | awk '/Keyboard K380 Keyboard/ {print $5}' \
-            | awk -F"id=" '{found=1} {print $2} END {if (!found) print "0"}')"
-
-    if [ $bluetoothKeyId -gt 0 ] 
-    then
-        xinput disable $internalKeyId
+while [ 1 ]; do
+    if [ $bluetooth_key_id -gt 0 ]; then
+        xinput disable $internal_key_id
     else
-        xinput enable $internalKeyId
+        xinput enable $internal_key_id
     fi
 done
